32247a6c58e09872bfbd4b427575526cf4113273
bistaprapannabista <bistaprapannabista@gmail.com>
2022-11-28 14:44:30 +0545

    middle commit

--- src/components/Note.js
+++ src/components/Note.js
@@ -6,6 +6,10 @@ const Note = (props) =>{
         <div className="card-body">
             <h5 className="card-title">{title}</h5>
             <p className="card-text">{description}</p>
+            <div class="d-flex justify-content-between">
+                <i class="fa-solid fa-trash"></i>
+                <i class="fa-solid fa-pen-to-square"></i>
+            </div>
         </div>
     </div>
     </div>
